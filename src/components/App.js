b6c8a5ee852973de2e494dbf4d42f949fa34fbee
Gonzalo Blasco <gonzoblasco@outlook.com>
2020-06-05 13:12:12 -0300

    División de Aplicación en Rutas

---
+++ src/components/App.js
@@ -0,0 +1,22 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Badges from '../pages/Badges';
+import BadgeNew from '../pages/BadgeNew';
+import Layout from './Layout';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Switch>
+          <Route exact path='/badges' component={ Badges } />
+          <Route exact path='/badges/new' component={ BadgeNew } />
+        </Switch>
+      </Layout>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/pages/BadgeNew.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+
+import './styles/BadgeNew.css';
+import header from '../images/badge-header.svg';
+import Badge from '../components/Badge';
+import BadgeForm from '../components/BadgeForm';
+
+class BadgeNew extends Component {
+  state = {
+    form: {
+      firstName: '',
+      lastName: '',
+      email: '',
+      jobTitle: '',
+      twitter: '',
+    },
+  };
+
+  handleChange = e => {
+    this.setState({
+      form: {
+        ...this.state.form,
+        [e.target.name]: e.target.value,
+      },
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <div className='BadgeNew__hero'>
+          <div className='BadgeNew__container'>
+            <img className='img-fluid BadgeNew_conf-logo' src={ header } alt='Logo' />
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-6">
+              <Badge
+                firstName={ this.state.form.firstName }
+                lastName={ this.state.form.lastName }
+                jobTitle={ this.state.form.jobTitle }
+                twitter={ this.state.form.twitter }
+                email={ this.state.form.email }
+                avatarUrl="https://2.gravatar.com/avatar/bfed63725b27aca51adf1f268754435f?s=400&d=mm"
+              />
+            </div>
+            <div className='col-6'>
+              <BadgeForm onChange={ this.handleChange } formValues={ this.state.form } />
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BadgeNew;
